a2747010fe558f9455d99e88883a6d2bd08082fe
ROBER XMX <35946602+roberdmc@users.noreply.github.com>
2022-06-28 17:38:17 -0300

    Test Data
    Test Data

---
+++ Testes pokemon dataset - Test data.py
@@ -0,0 +1,134 @@
+import tensorflow as tf
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+import sys
+import pickle
+import cv2
+from tqdm import tqdm
+import random
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from keras.models import load_model
+
+class_names = ['Abra', 'Alakazam', 'Blastoise', 'Bulbasaur', 'Charizard', 'Charmander', 'Charmeleon', 'Gastly', 
+               'Gengar', 'Haunter', 'Ivysaur', 'Kadabra', 'Pikachu', 'Raichu', 'Snorlax', 'Squirtle', 'Venusaur', 'Wartortle']
+
+model = load_model('modelPokemon.h5')
+print('Model Loaded!')
+model.sumary()
+
+model = model.load_weights('modelPokemonWeights.h5')
+print('Model Weights Loaded!')
+
+X_train = []
+y_train = []
+
+X_test = []
+y_test = []
+
+#Load train data
+pickle_in = open("X_train.pickle", "rb")
+X_train = pickle.load(pickle_in)
+
+pickle_in = open("y_train.pickle", "rb")
+y_train = pickle.load(pickle_in)
+
+#Load test data
+pickle_in = open("X_test.pickle", "rb")
+X_test = pickle.load(pickle_in)
+
+pickle_in = open("y_test.pickle", "rb")
+y_test = pickle.load(pickle_in)
+
+test_loss, test_acc = model.evaluate(X_test,  y_test, batch_size=16, verbose=2)
+
+print('\nTest accuracy:', test_acc)
+
+predictions = model.predict(X_test)
+
+predictions[0]
+
+np.argmax(predictions[0])
+
+y_test[0]
+
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+      color = 'blue'
+    else:
+      color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                  100*np.max(predictions_array),
+                                  class_names[true_label]),
+                                  color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array[i], true_label[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+  
+i = 0
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions, y_test, X-test)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions,  y_test)
+plt.show()
+
+i = 12
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions, y_test, X_test)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions,  y_test)
+plt.show()
+
+# Plota o primeiro X test images, e as labels preditas, e as labels verdadeiras.
+# Colore as predições corretas de azul e as incorretas de vermelho.
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions, y_test, X_test)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions, y_test)
+plt.show()
+
+# Grab an image from the test dataset.
+img = X_test[0]
+
+print(img.shape)
+
+# Adiciona a imagem em um batch que possui um só membro.
+img = (np.expand_dims(img,0))
+
+print(img.shape)
+
+predictions_single = model.predict(img)
+
+print(predictions_single)
+
+plot_value_array(0, predictions_single, y_test)
+_ = plt.xticks(range(10), class_names, rotation=45)
+
+np.argmax(predictions_single[0])
