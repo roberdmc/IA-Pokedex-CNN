ef263d085932aebee724797c3621e6a2b6ddb6bb
roberdmc <roberval.junior@RET-01042348>
2022-06-29 00:46:50 -0300

    Fix error of class

--- Testes pokemon dataset - Test data.py
+++ Testes pokemon dataset - Test data.py
@@ -11,29 +11,63 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from keras.models import load_model
+from numpy import random
 
-class_names = ['Abra', 'Alakazam', 'Blastoise', 'Bulbasaur', 'Charizard', 'Charmander', 'Charmeleon', 'Gastly', 
-               'Gengar', 'Haunter', 'Ivysaur', 'Kadabra', 'Pikachu', 'Raichu', 'Snorlax', 'Squirtle', 'Venusaur', 'Wartortle']
+class_names = ['Alakazam', 'Blastoise', 'Bulbasaur', 'Charizard', 'Charmander', 'Gastly', 
+               'Gengar', 'Pikachu', 'Snorlax', 'Squirtle', 'Venusaur']
+
+#class_names = ['Alakazam', 'Blastoise', 'Charizard',
+#               'Gengar', 'Pikachu', 'Venusaur']
+
+def get_dataset(path):
+    pass
 
 model = load_model('modelPokemon.h5')
 print('Model Loaded!')
 model.summary()
 
-#model.load_weights('modelPokemonWeights.h5')
-#print('Model Weights Loaded!')
+datadir_test = "dataset\\test_unitary\\"
 
-X_train = []
-y_train = []
+new_array =[]
+testing_data = []
+IMG_SIZE = 25
 
+#For test data
+for category in tqdm(class_names):
+    path = os.path.join(datadir_test, category) # path for each of the pokemons
+    class_num = class_names.index(category)
+    
+    # now we get each of them images
+    for img in os.listdir(path):
+        img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_COLOR)
+        new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+        testing_data.append([new_array, class_num])
+        #plt.imshow(new_array, cmap='gray')
+        #plt.show()
+        
+#For test data
 X_test = []
 y_test = []
 
-#Load train data
-pickle_in = open("X_train.pickle", "rb")
-X_train = pickle.load(pickle_in)
+#For test data
+for features, label in testing_data:
+    X_test.append(features)
+    y_test.append(label)
 
-pickle_in = open("y_train.pickle", "rb")
-y_train = pickle.load(pickle_in)
+X_test = np.array(X_test)
+y_test = np.array(y_test)
+
+#Dump for test data
+pickle_out = open("X_test.pickle", "wb")
+pickle.dump(X_test, pickle_out)
+pickle_out.close()
+
+pickle_out = open("y_test.pickle", "wb")
+pickle.dump(y_test, pickle_out)
+pickle_out.close()
+
+X_test = []
+y_test = []
 
 #Load test data
 pickle_in = open("X_test.pickle", "rb")
@@ -42,9 +76,9 @@ X_test = pickle.load(pickle_in)
 pickle_in = open("y_test.pickle", "rb")
 y_test = pickle.load(pickle_in)
 
-#test_loss, test_acc = model.evaluate(X_test,  y_test, batch_size=16, verbose=2)
+test_loss, test_acc = model.evaluate(X_test,  y_test, batch_size=64, verbose=2)
 
-#print('\nTest accuracy:', test_acc)
+print('\nTest accuracy:', test_acc)
 
 predictions = model.predict(X_test)
 
@@ -78,13 +112,14 @@ def plot_value_array(i, predictions_array, true_label):
   plt.grid(False)
   plt.xticks([])
   plt.yticks([])
-  thisplot = plt.bar(range(32), predictions_array, color="#777777")
+  thisplot = plt.bar(range(11), predictions_array, color="#777777")
   plt.ylim([0, 1])
   predicted_label = np.argmax(predictions_array)
 
   thisplot[predicted_label].set_color('red')
   thisplot[true_label].set_color('blue')
-  
+
+'''
 i = 0
 plt.figure(figsize=(6,3))
 plt.subplot(1,2,1)
@@ -92,43 +127,18 @@ plot_image(i, predictions, y_test, X_test)
 plt.subplot(1,2,2)
 plot_value_array(i, predictions,  y_test)
 plt.show()
-
-i = 12
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i, predictions, y_test, X_test)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions,  y_test)
-plt.show()
+'''
 
 # Plota o primeiro X test images, e as labels preditas, e as labels verdadeiras.
 # Colore as predições corretas de azul e as incorretas de vermelho.
-num_rows = 5
-num_cols = 3
+num_rows = 3
+num_cols = 4
 num_images = num_rows*num_cols
 plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+
 for i in range(num_images):
   plt.subplot(num_rows, 2*num_cols, 2*i+1)
   plot_image(i, predictions, y_test, X_test)
   plt.subplot(num_rows, 2*num_cols, 2*i+2)
   plot_value_array(i, predictions, y_test)
 plt.show()
-
-# Grab an image from the test dataset.
-img = X_test[0]
-
-print(img.shape)
-
-# Adiciona a imagem em um batch que possui um só membro.
-img = (np.expand_dims(img,0))
-
-print(img.shape)
-
-predictions_single = model.predict(img)
-
-print(predictions_single)
-
-plot_value_array(0, predictions_single, y_test)
-_ = plt.xticks(range(10), class_names, rotation=45)
-
-np.argmax(predictions_single[0])
--- Testes pokemon dataset.py
+++ Testes pokemon dataset.py
@@ -12,19 +12,20 @@ from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
 from keras.models import load_model
 
-class_names = ['Abra', 'Alakazam', 'Blastoise', 'Bulbasaur', 'Charizard', 'Charmander', 'Charmeleon', 'Gastly', 
-               'Gengar', 'Haunter', 'Ivysaur', 'Kadabra', 'Pikachu', 'Raichu', 'Snorlax', 'Squirtle', 'Venusaur', 'Wartortle']
+class_names = ['Alakazam', 'Blastoise', 'Bulbasaur', 'Charizard', 'Charmander', 'Gastly', 
+               'Gengar', 'Pikachu', 'Snorlax', 'Squirtle', 'Venusaur']
 
+#class_names = ['Alakazam', 'Blastoise', 'Charizard',
+#               'Gengar', 'Pikachu', 'Venusaur']
+               
 def get_dataset(path):
     pass
 
 datadir_train = "dataset\\train\\"
-datadir_test = "dataset\\test\\"
 
 new_array =[]
 training_data = []
-testing_data = []
-IMG_SIZE = 100
+IMG_SIZE = 25
 
 #For train data
 for category in tqdm(class_names):
@@ -33,22 +34,9 @@ for category in tqdm(class_names):
     
     # now we get each of them images
     for img in os.listdir(path):
-        img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+        img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_COLOR)
         new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
         training_data.append([new_array, class_num])
-        #plt.imshow(new_array, cmap='gray')
-        #plt.show()
-
-#For test data
-for category in tqdm(class_names):
-    path = os.path.join(datadir_test, category) # path for each of the pokemons
-    class_num = class_names.index(category)
-    
-    # now we get each of them images
-    for img in os.listdir(path):
-        img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
-        new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-        testing_data.append([new_array, class_num])
         #plt.imshow(new_array, cmap='gray')
         #plt.show()
 
@@ -56,25 +44,13 @@ for category in tqdm(class_names):
 X_train = []
 y_train = []
 
-#For test data
-X_test = []
-y_test = []
-
 #For train data
 for features, label in training_data:
     X_train.append(features)
     y_train.append(label)
 
-X_train = np.array(X_train).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+X_train = np.array(X_train)
 y_train = np.array(y_train)
-
-#For test data
-for features, label in testing_data:
-    X_test.append(features)
-    y_test.append(label)
-
-X_test = np.array(X_test).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-y_test = np.array(y_test)
 
 #Dump for train data
 pickle_out = open("X_train.pickle", "wb")
@@ -85,37 +61,11 @@ pickle_out = open("y_train.pickle", "wb")
 pickle.dump(y_train, pickle_out)
 pickle_out.close()
 
-#Open for train data
-pickle_in = open("X_train.pickle", "rb")
-X_train = pickle.load(pickle_in)
-
-pickle_in = open("y_train.pickle", "rb")
-y_train = pickle.load(pickle_in)
-
-#Dump for test data
-pickle_out = open("X_test.pickle", "wb")
-pickle.dump(X_test, pickle_out)
-pickle_out.close()
-
-pickle_out = open("y_test.pickle", "wb")
-pickle.dump(y_test, pickle_out)
-pickle_out.close()
-
-#Open for test data
-pickle_in = open("X_test.pickle", "rb")
-X_test = pickle.load(pickle_in)
-
-pickle_in = open("y_test.pickle", "rb")
-y_test = pickle.load(pickle_in)
-
 ## feed to CNN
-    
-X_train = X_train/255.0
-X_test = X_test/255.0
 
 model = Sequential()
 
-model.add(Conv2D(256, (3, 3), input_shape=(X_train.shape[1:])))
+model.add(Conv2D(256, (3, 3), input_shape=(IMG_SIZE, IMG_SIZE, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
@@ -125,21 +75,17 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
 
-model.add(Dense(64))
+model.add(Dense(256))
 
-model.add(Dense(32))
+model.add(Dense(11))
 model.add(Activation('sigmoid'))
 
 model.compile(loss='sparse_categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-model.fit(X_train, y_train, batch_size=64, epochs=10, validation_split=0.3)
+model.fit(X_train, y_train, batch_size=64, epochs=15, validation_split=0.3)
 
 #For save model
 model.save('modelPokemon.h5')
 print('Model Saved!')
-
-#For save model weights
-#model.save_weights('modelPokemonWeights.h5')
-#print('Model Weights Saved!')
