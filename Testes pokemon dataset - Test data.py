9801c62e2c8eb72250e7924d803eec3fcd502f1a
roberdmc <roberval.junior@RET-01042348>
2022-06-30 18:27:39 -0300

    Remove unused lines and change classes

--- Testes pokemon dataset - Test data.py
+++ Testes pokemon dataset - Test data.py
@@ -1,20 +1,16 @@
 import tensorflow as tf
 import numpy as np
-from matplotlib import pyplot as plt
 import os
 import sys
 import pickle
 import cv2
 from tqdm import tqdm
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
-from keras.models import load_model
-from numpy import random
+from matplotlib import pyplot as plt
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 
 #Define o nome das classes
-class_names = ['Arbok', 'Arcanine', 'Bellsprout', 'Blastoise', 'Butterfree', 'Charizard', 'Ditto', 'Gengar', 'Jigglypuff',  
-               'Machamp', 'Mankey', 'Metapod', 'Mewtwo', 'Parasect', 'Pikachu', 'Poliwag', 'Psyduck', 'Voltorb']
+class_names = ['Abra', 'Bulbasaur', 'Charmander', 'Gastly', 'Meowth', 'Pidgey', 'Pikachu', 'Squirtle', 'Staryu']
 
 def get_dataset(path):
     pass
@@ -25,13 +21,13 @@ print('Model Loaded!')
 model.summary()
 
 #Define o diretório do conjunto de testes
-datadir_test = "dataset\\test_17\\"
+datadir_test = "dataset_new\\test_17\\"
 
 new_array =[]
 testing_data = []
 
 #Define o tamanho em altura e largura para o redimensiomanento das imagens
-IMG_SIZE = 75
+IMG_SIZE = 70
 
 #Gera o testing_data para o conjunto de testes
 for category in tqdm(class_names):
@@ -43,7 +39,6 @@ for category in tqdm(class_names):
         img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_COLOR)
         new_array = cv2.resize(img_array[:,:,::-1], (IMG_SIZE, IMG_SIZE))
         testing_data.append([new_array, class_num])
-
 
 #Cria os vetores para as imagens "X" e labels "y"
 X_test = []
@@ -57,17 +52,6 @@ for features, label in testing_data:
 #Converte os vetores de imagens e labels para np array
 X_test = np.array(X_test)
 y_test = np.array(y_test)
-
-'''
-#Exporta os dados de teste (imagens e labels)
-pickle_out = open("exported_files\\X_test.pickle", "wb")
-pickle.dump(X_test, pickle_out)
-pickle_out.close()
-
-pickle_out = open("exported_files\\y_test.pickle", "wb")
-pickle.dump(y_test, pickle_out)
-pickle_out.close()
-'''
 
 #Calcula a acurácia para o conjunto de testes
 test_loss, test_acc = model.evaluate(X_test,  y_test, batch_size=64, verbose=2)
@@ -99,17 +83,6 @@ def plot_image(i, predictions_array, true_label, img):
                                   class_names[true_label]),
                                   color=color)
 
-#Plota uma unica imagem
-'''
-i = 0
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i, predictions, y_test, X_test)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions,  y_test)
-plt.show()
-'''
-
 #Plota as primeiras imagens de teste, as labels preditas, as labels verdadeiras e a acurácia.
 num_rows = 6
 num_cols = 6
@@ -119,5 +92,5 @@ plt.figure(figsize=(2*2*num_cols, 2*num_rows))
 
 for i in range(num_images):
   plt.subplot(num_rows, 2*num_cols, 2*i+1)
-  plot_image(i+747, predictions, y_test, X_test)
-plt.show()
+  plot_image(i, predictions, y_test, X_test)
+#plt.show()
--- Testes pokemon dataset.py
+++ Testes pokemon dataset.py
@@ -1,31 +1,28 @@
 import tensorflow as tf
 import numpy as np
-from matplotlib import pyplot as plt
 import os
 import sys
 import pickle
 import cv2
 from tqdm import tqdm
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
-from keras.models import load_model
+from matplotlib import pyplot as plt
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
 
 #Define o nome das classes
-class_names = ['Arbok', 'Arcanine', 'Bellsprout', 'Blastoise', 'Butterfree', 'Charizard', 'Ditto', 'Gengar', 'Jigglypuff',  
-               'Machamp', 'Mankey', 'Metapod', 'Mewtwo', 'Parasect', 'Pikachu', 'Poliwag', 'Psyduck', 'Voltorb']
+class_names = ['Abra', 'Bulbasaur', 'Charmander', 'Gastly', 'Meowth', 'Pidgey', 'Pikachu', 'Squirtle', 'Staryu']
               
 def get_dataset(path):
     pass
 
 #Define o diretório do conjunto de treinamento
-datadir_train = "dataset\\train_17\\"
+datadir_train = "dataset_new\\train_17\\"
 
 new_array =[]
 training_data = []
 
 #Define o tamanho em altura e largura para o redimensiomanento das imagens
-IMG_SIZE = 75
+IMG_SIZE = 100
 
 #Gera o training_data para o conjunto de treinamento
 for category in tqdm(class_names):
@@ -52,17 +49,6 @@ for features, label in training_data:
 X_train = np.array(X_train)
 y_train = np.array(y_train)
 
-'''
-#Exporta os dados de treino (imagens e labels)
-pickle_out = open("exported_files\\X_train.pickle", "wb")
-pickle.dump(X_train, pickle_out)
-pickle_out.close()
-
-pickle_out = open("exported_files\\y_train.pickle", "wb")
-pickle.dump(y_train, pickle_out)
-pickle_out.close()
-'''
-
 ##Processamento CNN
 model = Sequential()
 
@@ -75,12 +61,15 @@ model.add(Conv2D(128, (3, 3), activation = 'relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Conv2D(256, (3, 3), activation = 'relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(512, (3, 3), activation = 'relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Converte feature maps 3D para feature vectors 1D
 model.add(Flatten())  
 
 #Hidden Layer
-model.add(Dense(512, activation = 'relu'))
+model.add(Dense(1024, activation = 'relu'))
+model.add(Dense(1024, activation = 'relu'))
 
 #Output Layer
 model.add(Dense(len(class_names)))
@@ -90,7 +79,7 @@ model.compile(loss='sparse_categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-model.fit(X_train, y_train, batch_size=128, epochs=100, validation_split=0.3)
+model.fit(X_train, y_train, batch_size=128, epochs=20, validation_split=0.3)
 
 #Salva o modelo gerado e treinado
 model.save('exported_files\\modelPokemon.h5')
